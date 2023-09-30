482e840f4c8a6ccf07b7c74642691d2b256427cb
Yuanqing <yl11249@nyu.edu>
2023-09-29 23:31:07 -0400

    Cookie Decoration

--- 0/sketch.js
+++ 0/sketch.js
@@ -2,19 +2,31 @@ let colors = [
   { r: 252, g: 57, b: 131 },
   { r: 125, g: 216, b: 253 },
   { r: 255, g: 224, b: 95 },
-  { r: 253, g: 168, b: 5 },
+  { r: 255, g: 255, b: 255 },
   { r: 255, g: 191, b: 217 }
 ];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background('ivory');
+
+  //draw the plate and cookies
+  let plateColor = { r: 253, g: 138, b: 172 };
+  drawCircle(width / 2, height / 2, height, plateColor);
+  let cookieColor = { r: 253, g: 168, b: 5 };
+  drawCircle(width / 2, height / 2, 300, cookieColor);
+  drawCircle(width / 2 - 310, height / 2, 300, cookieColor);
+  drawCircle(width / 2 + 310, height / 2, 300, cookieColor);
+  drawCircle(width / 2 + 160, height / 2 + 280, 300, cookieColor);
+  drawCircle(width / 2 - 160, height / 2 + 280, 300, cookieColor);
+  drawCircle(width / 2 + 160, height / 2 - 280, 300, cookieColor);
+  drawCircle(width / 2 - 160, height / 2 - 280, 300, cookieColor);
 }
 
+//mouseClick to generate random small shapes
 function mousePressed() {
-  let size = random(20, 50); 
+  let size = random(5, 30);
   let randomColor = random(colors);
-
   if (random() < 0.333) {
     drawCircle(mouseX, mouseY, size, randomColor);
   } else if (random() < 0.666) {
@@ -25,32 +37,44 @@ function mousePressed() {
 }
 
 function drawCircle(x, y, size, color) {
-  noStroke(); 
-  fill(color.r, color.g, color.b); 
+  noStroke();
+  fill(color.r, color.g, color.b);
   ellipse(x, y, size, size);
 }
 
 function drawRectangle(x, y, size, color) {
-  noStroke(); 
-  fill(color.r, color.g, color.b); 
+  noStroke();
+  fill(color.r, color.g, color.b);
   rect(x, y, size, size);
 }
 
 function drawTriangle(x, y, size, color) {
-  noStroke(); 
-  fill(color.r, color.g, color.b); 
+  noStroke();
+  fill(color.r, color.g, color.b);
   triangle(x, y, x + size, y, x + size / 2, y + size);
 }
 
 function keyReleased() {
-  if (key === 's' || key === 'S') {''
+  if (key === 's' || key === 'S') {
+    ''
     saveCanvas('myCanvas', 'png');
   }
 }
 
-function keyPressed(){
-  if(key == "r") {
+function keyPressed() {
+  if (key == "r") {
     background('ivory');
+
+    let plateColor = { r: 253, g: 138, b: 172 };
+    drawCircle(width / 2, height / 2, height, plateColor);
+    let cookieColor = { r: 253, g: 168, b: 5 };
+    drawCircle(width / 2, height / 2, 300, cookieColor);
+    drawCircle(width / 2 - 310, height / 2, 300, cookieColor);
+    drawCircle(width / 2 + 310, height / 2, 300, cookieColor);
+    drawCircle(width / 2 + 160, height / 2 + 280, 300, cookieColor);
+    drawCircle(width / 2 - 160, height / 2 + 280, 300, cookieColor);
+    drawCircle(width / 2 + 160, height / 2 - 280, 300, cookieColor);
+    drawCircle(width / 2 - 160, height / 2 - 280, 300, cookieColor);
   }
 }
 
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 #0 file for HW04B  
 Project overview: In this project, I aim to create an engaging and interactive canvas that allows users to easily decorate a virtual sugar cookie with random shapes. Users can click the mouse to add various colorful and fun shapes to their sugar cookie, providing a delightful and creative experience.  
 
+Inspiration:  
+![Inspiration](./0Inspiration.jpg) 
+
 Key Features:
 1. Interactive Canvas: The canvas will provide an interactive environment where users can decorate their sugar cookie.
 2. Random Shapes: Users will have random shapes by simply clicking the mouse.
@@ -11,13 +14,24 @@ Key Features:
 Thinking Process:
 ![Process](./0process.jpg)  
 
-Hard Part:
-Generate random colors from the designed palette.  
+Hard Part:  
 
-I understood the way to generate random colors. In this project, I wanna try to select a random color from the designed palette to fill shapes. However, it's a little challenging to write if() to fill different colors, since there are too many function, including draw 3 different shapes, and mouseClick() function.
+1. Generate random colors from the designed palette. I understood the way to generate random colors. In this project, I wanna try to select a random color from the designed palette to fill shapes. However, it's a little challenging to write if() to fill different colors, since there are too many function, including draw 3 different shapes, and mouseClick() function.
+
+2. Make cookie shapes behind the small decorations. First, I used ellipse() in function draw(), but the cookie shape is above the small decorations. Like this:  
+![Wrong1](./Wrong1.png)  
+
+Later, I figured it out by using drawCircle() in function setup().
 
 Fun Part:
-Draw a random shape. 
+Draw a random shape. It's efficient to create function to draw 3 shapes. And then use if(), where the way used in Random Lottery example, to achieve the feature.  
 
-It's efficient to create function to draw 3 shapes. And then use if(), where the way used in Random Lottery example, to achieve the feature.
+Final outcome:
+![SavedCanvas](./myCanvas.png)  
+
+#1 file for personal practice  
+Project overview: In this project, I found an illustration design featuring a blend of geometric shapes. My goal is to recreate these intricate combinations of shapes and implement the mouseMove() function to enable color changes when hovering over each shape.
+
+Reference:
+![Reference](./IMG_5084.jpg)
 
